ad194fa492ab3d40eb773247ea14192449c8434d
Reed Streets <rstreets96@gmail.com>
2022-12-11 19:44:01 -0800

    Day 1 Part 1 completed

---
+++ SumCalories.py
@@ -0,0 +1,31 @@
+def splitByElf(data):
+    parsedData = [[]]
+    for item in data:
+        if item == '':
+            parsedData.append([])
+        else:
+            parsedData[-1].append(item)
+    return parsedData
+
+def sumEachElf(data):
+    elfSums = []
+    for elf in data:
+        sum = 0
+        for calories in elf:
+            sum += int(calories)
+        elfSums.append(sum)
+    return(elfSums)
+
+def findGreatest(data):
+    greatest = 0
+    for elf in data:
+        if(elf > greatest):
+            greatest = elf
+    return greatest
+
+fileObject = open("InputFiles/Day1.txt", "r")
+dataLines = fileObject.read().splitlines()
+dataPerElf = splitByElf(dataLines)
+elfSums = sumEachElf(dataPerElf)
+greatestElf = findGreatest(elfSums)
+print(greatestElf)

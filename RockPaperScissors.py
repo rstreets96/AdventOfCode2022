21a7d8f0ce41df9b4bc53a52f3f7ccd660aee6d1
Reed Streets <rstreets96@gmail.com>
2022-12-12 18:24:11 -0800

    Completed Day 2

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,10 +1,27 @@
 def linesToInputs(data):
-    for line in data:
-       line = line.split(' ')
-    return data
+    output = [[]]
+    output = [line.split(' ') for line in data]
+    return output
+
+def pointsWon(data):
+    lookUpWin = {'A':{'X':4, 'Y':8, 'Z':3}, 'B':{'X':1, 'Y':5, 'Z':9}, 'C':{'X':7, 'Y':2, 'Z':6}}
+    points = 0
+    for game in data:
+        points += lookUpWin[game[0]][game[1]]
+    return points
+
+def pickMyInput(data):
+    lookUpInput = {'A':{'X':'Z', 'Y':'X', 'Z':'Y'}, 'B':{'X':'X', 'Y':'Y', 'Z':'Z'}, 'C':{'X':'Y', 'Y':'Z', 'Z':'X'}}
+    output = []
+    for game in data:
+        game[1] = lookUpInput[game[0]][game[1]]
+        output.append(game)
+    return output
 
 
 fileObject = open("InputFiles/Day2.txt", "r")
 dataLines = fileObject.read().splitlines()
 inputs = linesToInputs(dataLines)
-print(inputs)
+newInputs = pickMyInput(inputs)
+points = pointsWon(newInputs)
+print(points)

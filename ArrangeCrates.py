fd88dc8eb6a9375f92eced09eb76e67713ff5d38
Reed Streets <rstreets96@gmail.com>
2022-12-14 14:40:32 -0800

    Both parts completed for Day 5

--- ArrangeCrates.py
+++ ArrangeCrates.py
@@ -1,4 +1,6 @@
-def parsingInput(data):
+import numpy as np
+
+def parsingStartPositions(data):
     output = []
     for line in data:        
         if(line == ' 1   2   3   4   5   6   7   8   9 '):
@@ -7,13 +9,61 @@ def parsingInput(data):
         output.append(line)
     return output
 
+def rowsToColumns(data):
+    transposed = np.array(data).T.tolist()
+    for line in transposed:
+        line.reverse()
+        while '   ' in line:
+            line.remove('   ')
+    return transposed
 
+def parsingMoves(data):
+    output = []
+    startMoves = False
+    for line in data:
+        if(not startMoves):
+            if(line == ''):
+                startMoves = True
+            else:
+                continue
+        else:
+            output.append(line.split(' '))
+    return output
 
+###############################################################################
+#Part One Functions
+###############################################################################
+def move1Crate(positionData, startColumn, endColumn):
+    positionData[endColumn].append(positionData[startColumn][-1])
+    positionData[startColumn].pop(-1)
+
+def moveCrates(positionData, move):
+    numMoves = int(move[1])
+    startColumn = int(move[3]) - 1
+    endColumn = int(move[5]) - 1
+    for move in range(0, numMoves):
+        move1Crate(positionData, startColumn, endColumn)
+
+###############################################################################
+#Part Two Functions
+###############################################################################
+def moveMultipleCrates(positionData, move):
+    numCrates = int(move[1])  
+    startColumn = int(move[3]) - 1
+    endColumn = int(move[5]) - 1
+    positionData[endColumn].extend(positionData[startColumn][-numCrates:])
+    del positionData[startColumn][-numCrates:]
+    # for line in positionData:
+    #     print(line)
 
 if __name__ == '__main__':
     fileObject = open("InputFiles/Day5.txt", "r")
     dataLines = fileObject.read().splitlines()
-    parsedData = parsingInput(dataLines)
-    #crateColumns = originalState(parsedData)
-    for line in parsedData:
+    startPositions = parsingStartPositions(dataLines)
+    columns = rowsToColumns(startPositions)
+    moves = parsingMoves(dataLines)
+    for move in moves:
+        moveMultipleCrates(columns, move)
+    #moveMultipleCrates(columns, moves[0])
+    for line in columns:
         print(line)

71a3d701d6e436e68698de5237fb4cda78bdb547
Reed Streets <rstreets96@gmail.com>
2022-12-16 18:14:04 -0800

    Day 9 Part 1 Complete

--- RopeMovements.py
+++ RopeMovements.py
@@ -1,4 +1,64 @@
+###############################################################################
+#Class used to hold a coordinate pair
+###############################################################################
+class CoordPosition():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
+###############################################################################
+#Function to handle one line of movement input
+###############################################################################
+
+def doOneMoveSet(moveSet, head, tail):
+    direction = 0
+    up = 1
+    down = 2
+    left = 3
+    right = 4
+    tailMoves = {-2:{-2:[-1, -1], -1:[-1, -1], 0:[-1, 0], 1:[-1, 1], 2:[-1, 1]}, 
+                -1:{-2:[-1, -1], -1:[0, 0], 0:[0, 0], 1:[0, 0], 2:[-1, 1]}, 
+                0:{-2:[0, -1], -1:[0, 0], 0:[0, 0], 1:[0, 0], 2:[0, 1]}, 
+                1:{-2:[1, -1], -1:[0, 0], 0:[0, 0], 1:[0, 0], 2:[1, 1]}, 
+                2:{-2:[1, -1], -1:[1, -1], 0:[1, 0], 1:[1, 1], 2:[1, 1]}}
+
+    if(moveSet[0] == 'U'):
+        direction = up
+    elif(moveSet[0] == 'D'):
+        direction = down
+    elif(moveSet[0] == 'L'):
+        direction = left
+    elif(moveSet[0] == 'R'):
+        direction = right   
+    for i in range(0, int(moveSet[1])):
+        if(direction == up):
+            head.y += 1
+        elif(direction == down):
+            head.y -= 1
+        elif(direction == left):
+            head.x -= 1
+        elif(direction == right):
+            head.x += 1
+        yDiff = head.y - tail.y
+        xDiff = head.x - tail.x
+
+        tail.x += tailMoves[xDiff][yDiff][0]
+        tail.y += tailMoves[xDiff][yDiff][1]
+
+        tailSpot = CoordPosition(tail.x, tail.y)
+        newSpot = True
+        for tailPos in tailPosList:
+            if((tailPos.x == tailSpot.x) and (tailPos.y == tailSpot.y)):
+                newSpot = False
+        if(newSpot):
+            tailPosList.append(tailSpot)
+    return(head, tail)
+
+###############################################################################
+#Global Variable to hold all unique Tail coordinates
+###############################################################################
+
+tailPosList = []
 
 if __name__ == '__main__':
     fileObject = open("InputFiles/Day9.txt", "r")
@@ -7,8 +67,15 @@ if __name__ == '__main__':
     for line in dataLines:
         moves.append(line.split(' '))
 
-    originShiftx = 0
-    originShifty = 0
+###############################################################################
+#Execution for Part 1
+###############################################################################
 
+    headPos = CoordPosition(0, 0)
+    tailPos = CoordPosition(0, 0)
     for move in moves:
-        print(move)
+        headPos, tailPos = doOneMoveSet(move, headPos, tailPos)
+
+    print(len(tailPosList))
+
+

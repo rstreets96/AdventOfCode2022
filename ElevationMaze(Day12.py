de7112506a3e9519e8e133f89c9a25955e78bd7b
Reed Streets <rstreets96@gmail.com>
2022-12-20 14:26:23 -0800

    Completed Day 12 Part 2

--- ElevationMaze(Day12.py
+++ ElevationMaze(Day12.py
@@ -1,6 +1,10 @@
 import numpy as np
 import sys
 
+###############################################################################
+#Dictionary to translate elevations from letters to numbers
+############################################################################### 
+
 elevationDict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 
                 'f':6, 'g':7, 'h':8, 'i':9, 'j':10, 
                 'k':11, 'l':12, 'm':13, 'n':14, 'o':15, 
@@ -9,6 +13,10 @@ elevationDict = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5,
                 'S':1, 'E':26}
             
 
+###############################################################################
+#Class used to track the distance from start for each (x,y) coordinate
+###############################################################################            
+
 class Node():
     def __init__(self, x, y, elevation, distance):
         self.x = x
@@ -16,37 +24,74 @@ class Node():
         self.elevation = elevation
         self.distance = distance
 
+###############################################################################
+#Function to build a list of nodes from the grid input
+###############################################################################  
+
 def buildNodes(grid):
     output = []
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if(grid[x][y] == 'S'):
-                startNode = Node(x, y, grid[x][y], 0)
-                output.append(startNode)
+                if(startAtEnd):
+                    startNode = Node(x, y, grid[x][y], sys.maxsize)
+                    output.append(startNode)
+                else:
+                    startNode = Node(x, y, grid[x][y], 0)
+                    output.append(startNode)
             elif(grid[x][y] == 'E'):
-                endNode = Node(x, y, grid[x][y], sys.maxsize)
-                output.append(endNode)
+                if(startAtEnd):
+                    endNode = Node(x, y, grid[x][y], 0)
+                    output.append(endNode)
+                else:
+                    endNode = Node(x, y, grid[x][y], sys.maxsize)
+                    output.append(endNode)
             else:
                 output.append(Node(x, y, grid[x][y], sys.maxsize))
     return output, startNode, endNode
 
+###############################################################################
+#Function to 'visit' one node and change the distances for adjacent nodes
+############################################################################### 
+
 def visitNode(unvisitedNodes, visitedNodes, currentNode):
-    for node in unvisitedNodes:
-        if(node.x == currentNode.x and (node.y == currentNode.y + 1 or node.y == currentNode.y - 1) and (elevationDict[node.elevation] <= elevationDict[currentNode.elevation] + 1)):
-            if(currentNode.distance + 1 < node.distance):
-                node.distance = currentNode.distance + 1
-        elif(node.y == currentNode.y and (node.x == currentNode.x + 1 or node.x == currentNode.x - 1)) and (elevationDict[node.elevation] <= elevationDict[currentNode.elevation] + 1):
-            if(currentNode.distance + 1 < node.distance):
-                node.distance = currentNode.distance + 1
+    if(startAtEnd):
+        for node in unvisitedNodes:
+            if(node.x == currentNode.x and (node.y == currentNode.y + 1 or node.y == currentNode.y - 1) and (elevationDict[node.elevation] >= elevationDict[currentNode.elevation] - 1)):
+                if(currentNode.distance + 1 < node.distance):
+                    node.distance = currentNode.distance + 1
+            elif(node.y == currentNode.y and (node.x == currentNode.x + 1 or node.x == currentNode.x - 1)) and (elevationDict[node.elevation] >= elevationDict[currentNode.elevation] - 1):
+                if(currentNode.distance + 1 < node.distance):
+                    node.distance = currentNode.distance + 1
+    else:
+        for node in unvisitedNodes:
+            if(node.x == currentNode.x and (node.y == currentNode.y + 1 or node.y == currentNode.y - 1) and (elevationDict[node.elevation] <= elevationDict[currentNode.elevation] + 1)):
+                if(currentNode.distance + 1 < node.distance):
+                    node.distance = currentNode.distance + 1
+            elif(node.y == currentNode.y and (node.x == currentNode.x + 1 or node.x == currentNode.x - 1)) and (elevationDict[node.elevation] <= elevationDict[currentNode.elevation] + 1):
+                if(currentNode.distance + 1 < node.distance):
+                    node.distance = currentNode.distance + 1
     visitedNodes.append(currentNode)
     unvisitedNodes.remove(currentNode)
 
+###############################################################################
+#Function to choose the new current node via smallest distance
+############################################################################### 
+
 def newCurrentNode(unvisitedNodes):
-    newNode = Node(0, 0, 'a', sys.maxsize)
+    newNode = Node(-1, -1, 'a', sys.maxsize)
     for node in unvisitedNodes:
         if(node.distance < newNode.distance):
             newNode = node
+    if(newNode.x == -1):
+        print('Fuck')
+        unvisitedNodes = []
     return newNode
+
+###############################################################################
+#Bool to choose between part 1 and part 2
+###############################################################################
+startAtEnd = True
 
 if __name__ == '__main__':
     fileObject = open("InputFiles/Day12.txt", "r")
@@ -54,19 +99,40 @@ if __name__ == '__main__':
     gridInv = []
     for line in dataLines:
         gridInv.append(list(line))
-    
     grid = np.array(gridInv).T.tolist()
-    
     unvisitedNodes, startNode, endNode = buildNodes(grid)
     visitedNodes = []
-    # currentNode = startNode
 
-    while(endNode not in visitedNodes):
+###############################################################################
+#Execution for Part 1
+###############################################################################
+    # while(endNode not in visitedNodes):
+    #     currentNode = newCurrentNode(unvisitedNodes)
+    #     visitNode(unvisitedNodes, visitedNodes, currentNode)
+
+    # print(currentNode.distance)
+
+
+###############################################################################
+#Execution for Part 2
+###############################################################################
+    while(unvisitedNodes):
         currentNode = newCurrentNode(unvisitedNodes)
-        visitNode(unvisitedNodes, visitedNodes, currentNode)
-        
+        if(currentNode.x == -1):
+            break
+        visitNode(unvisitedNodes, visitedNodes, currentNode) 
+        print(len(unvisitedNodes))  
+    
+    closestADist = sys.maxsize
+    for node in visitedNodes:
+        if(node.elevation == 'a' or node.elevation == 'S'):
+            if(node.distance < closestADist):
+                print(node.distance)
+                closestADist = node.distance
 
-    print(currentNode.distance)
-    print(currentNode.x)
-    print(currentNode.y)
-    print(currentNode.elevation)
+    print(closestADist)
+
+    # print(currentNode.distance)
+    # print(currentNode.x)
+    # print(currentNode.y)
+    # print(currentNode.elevation)
